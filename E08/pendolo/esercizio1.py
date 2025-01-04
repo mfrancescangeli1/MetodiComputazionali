9b52a5b5c55686b3ea770e48f658b2214c239cc3
mfrancescangeli1 <matfra@DESKTOP-J0490I0.com>
2025-01-04 17:57:25 +0100

    aggiornamento pendolo E08

--- E08/pendolo/esercizio1.py
+++ E08/pendolo/esercizio1.py
@@ -4,17 +4,29 @@ import pandas as pd
 from scipy import optimize
 import matplotlib.pyplot as plt
 from scipy import integrate
-def drdt_pendolo(r, t, l,g):
+def drdt_pendolo(r, t, l):
     dthdt=r[1]
-    dwdt=(-g/l)*np.sin(r[0])
+    dwdt=(-9.81/l)*np.sin(r[0])
     return (dthdt,dwdt)
-time_vec = np.linspace(0, 10, 100)
-l=0.5
-g=9.81
-th0=3.14/4
-yinit = ( th0,0)
-yarr  = integrate.odeint(drdt_pendolo, yinit, time_vec, args=(l,g))
+
+l1=0.5 
+l2=1.0
+theta0 = np.radians(45)
+omega0 = 0
+yinit = (theta0, omega0)
+yinit1 = (theta0*2/3, omega0)
+dt = 0.01
+ptimes = np.arange(0, 5, dt)
+
+yarr1  = integrate.odeint(drdt_pendolo, yinit,ptimes, args=(l1,))
+yarr2  = integrate.odeint(drdt_pendolo, yinit1,ptimes, args=(l1,))
+yarr3  = integrate.odeint(drdt_pendolo, yinit,ptimes, args=(l2,))
+
 fig,ax = plt.subplots(figsize=(9,6))
-plt.plot(time_vec,  yarr, label=('x', 'v'))
-plt.legend(fontsize=14)
+plt.plot(ptimes,  np.degrees(yarr1[:,0]), label='l1,th0')
+plt.plot(ptimes, np.degrees(yarr2[:,0]), label='l1,th1')
+plt.plot(ptimes,  np.degrees(yarr3[:,0]), label='l2,th0')
+plt.xlabel('t')
+plt.ylabel(r'$\theta$  [deg]')
+plt.legend()
 plt.show()
