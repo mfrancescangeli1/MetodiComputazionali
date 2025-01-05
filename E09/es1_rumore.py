88a5b231cfee4dd5b77107e742990fb7c4724e6e
mfrancescangeli1 <matfra@DESKTOP-J0490I0.com>
2025-01-05 11:27:12 +0100

    aggiornamento E09- es2: non finito

--- E09/es1_rumore.py
+++ E09/es1_rumore.py
@@ -1,46 +1,86 @@
 import numpy as np
 import pandas as pd
-
 from scipy import constants, fft
 import scipy
 import matplotlib.pyplot as plt
-data1=pd.read_csv('https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2024/refs/heads/main/dati/trasformate_fourier/data_sample1.csv')
-data2=pd.read_csv('https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2024/refs/heads/main/dati/trasformate_fourier/data_sample2.csv')
-data3=pd.read_csv('https://raw.githubusercontent.com/s-germani/metodi-computazionali-fisica-2024/refs/heads/main/dati/trasformate_fourier/data_sample3.csv')
-fig, ax = plt.subplots()
+data1=pd.read_csv('data_sample1.csv')
+data2=pd.read_csv('data_sample2.csv')
+data3=pd.read_csv('data_sample3.csv')
 '''
-ax.plot(data1['time'], data1['meas'], label='data 1')
-ax.plot(data2['time'], data2['meas'],label='data2')
-ax.plot(data3['time'], data3['meas'],label='data3')
-ax.set_xlabel('Time (s)')
-ax.set_ylabel('meas')
+grafico segnali
+'''
+fig,ax = plt.subplots(figsize=(9,6))
 
-'''
+plt.plot(data1['time'], data1['meas'], color='cyan',      label='Sample 1')
+plt.plot(data2['time'], data2['meas'], color='limegreen', label='Sample 2')
+plt.plot(data3['time'], data3['meas'], color='orange',    label='Sample 3')
+plt.legend()
+plt.xlabel('time [s]')
+plt.xlabel('Signal')
+plt.show()
 def rumore(freq,k,beta):
     return k/(freq**beta)
 #analisi di Fourier
+dt1=data1['time'][1]-data1['time'][0]
 c1  = fft.fft(data1['meas'].values)
-freqs1 = fft.fftfreq(len(data1), d=1)
-c2  = fft.rfft(data2['meas'].values)
-freqs2 = fft.rfftfreq(len(data2), d=1)
-c3  = fft.rfft(data3['meas'].values)
-freqs3 = fft.rfftfreq(len(data3), d=1)
-pnames=['k','beta']
+freqs1 = fft.fftfreq(len(data1), d=dt1)
+dt2=data2['time'][1]-data2['time'][0]
+c2  = fft.fft(data2['meas'].values)
+freqs2 = fft.fftfreq(len(data2), d=dt2)
+dt3=data3['time'][1]-data3['time'][0]
+c3  = fft.fft(data3['meas'].values)
+freqs3 = fft.fftfreq(len(data3), d=dt3)
+#grafico spettro di potenza
+fig,ax = plt.subplots(figsize=(9,6))
+plt.plot( freqs1[:len(c1)//2], np.absolute(c1[:len(c1)//2])**2,
+          color='cyan',label='Sample 1')
+plt.plot( freqs2[:len(c2)//2], np.absolute(c2[:len(c2)//2])**2,
+          color='limegreen', label='Sample 2')
+plt.plot( freqs3[:len(c3)//2], np.absolute(c3[:len(c3)//2])**2,
+          color='orange', label='Sample 3')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('f [Hz]')
+plt.ylabel(r'$\left| c_k\right|^2$')
+plt.legend()
+plt.show()
+
 #fit dei dati
 params1, params_covariance1=scipy.optimize.curve_fit(
-    rumore,freqs1[3:len(data1)//2],c1[3:len(data1)//2],p0=[1, 1])
-
+    rumore,freqs1[2:len(c1)//2],np.absolute(c1[2:len(c1)//2])**2, p0=[1, 1])
 print(params1,params_covariance1.diagonal())
-plt.plot(freqs1[3:len(data1['meas'])//2],
-         np.absolute(c1[3:len(data1)//2])**2, label='data1')
-plt.plot(freqs1[3:len(data1['meas'])//2],
-         rumore(freqs1[3:len(data2)//2],params1[0],params1[1]), label='fit')
-'''
-plt.plot(freqs2[:len(data2['meas'])//2],
-         np.absolute(c2[:len(data1)//2])**2, label='data2')
-plt.plot(freqs3[:len(data3['meas'])//2],
-         np.absolute(c3[:len(data1)//2])**2, label='data3')
-'''
+fig,ax = plt.subplots(figsize=(9,6))
+plt.plot(freqs1[:len(c1)//2],
+         np.absolute(c1[:len(c1)//2])**2,color='orange', label='data1')
+plt.plot(freqs1[1:len(c1)//2],
+         rumore(freqs1[1:len(c1)//2],params1[0],params1[1]),
+         color='darkred', label='fit')
+plt.xlabel('Freq. [Hz]')
+plt.ylabel(r'$\left| c_k \right|^2$')
+plt.xscale('log')
+plt.yscale('log')
+plt.legend(loc='lower left')
+plt.show()
+params2, params_covariance2=scipy.optimize.curve_fit(
+    rumore,freqs2[5:len(c2)//2],np.absolute(c2[5:len(c2)//2])**2, p0=[1, 1])
+plt.plot(freqs2[:len(c2)//2],
+         np.absolute(c2[:len(c2)//2])**2,color='cyan', label='data2')
+plt.plot(freqs2[1:len(c2)//2],
+         rumore(freqs2[1:len(c2)//2],params2[0],params2[1]),
+         color='darkblue', label='fit')
+plt.xlabel('Freq. [Hz]')
+plt.ylabel(r'$\left| c_k \right|^2$')
+plt.xscale('log')
+plt.yscale('log')
+plt.legend(loc='lower left')
+plt.show()
+params3, params_covariance3=scipy.optimize.curve_fit(
+    rumore,freqs3[5:len(c3)//2],np.absolute(c3[5:len(c3)//2])**2, p0=[1, 1])
+plt.plot(freqs3[:len(c3)//2],
+         np.absolute(c3[:len(c3)//2])**2,color='yellow', label='data3')
+plt.plot(freqs3[1:len(c3)//2],
+         rumore(freqs3[1:len(c3)//2],params3[0],params3[1]),
+         color='gold', label='fit')
 plt.xlabel('Freq. [Hz]')
 plt.ylabel(r'$\left| c_k \right|^2$')
 plt.xscale('log')
--- E09/es2_inquinanti.py
+++ E09/es2_inquinanti.py
@@ -6,36 +6,45 @@ data=pd.read_csv('copernicus_PG_selected.csv')
 
 '''
 tutti i graficini
-plt.plot(data['date'],data['mean_co_ug/m3'], label='CO')
-plt.plot(data['date'],data['mean_nh3_ug/m3'], label='NH3')
-plt.plot(data['date'],data['mean_no2_ug/m3'], label='NO2')
-plt.plot(data['date'],data['mean_o3_ug/m3'], label='03')
-plt.plot(data['date'],data['mean_pm10_ug/m3'], label='pm10')
-plt.plot(data['date'],data['mean_pm2p5_ug/m3'], label='pm2p5')
+'''
+plt.plot(data['date']-data['date'][0],data['mean_co_ug/m3'], label='CO')
+plt.plot(data['date']-data['date'][0],data['mean_nh3_ug/m3'], label='NH3')
+plt.plot(data['date']-data['date'][0],data['mean_no2_ug/m3'], label='NO2')
+plt.plot(data['date']-data['date'][0],data['mean_o3_ug/m3'], label='03')
+plt.plot(data['date']-data['date'][0],data['mean_pm10_ug/m3'], label='pm10')
+plt.plot(data['date']-data['date'][0],data['mean_pm2p5_ug/m3'], label='pm2p5')
 plt.xlabel('Freq. [Hz]')
 plt.ylabel('inquinanti')
 plt.xscale('log')
 plt.yscale('log')
 plt.legend(loc='lower left')
 plt.show()
-'''
+#analisi CO
+
 c = fft.rfft(data['mean_co_ug/m3'].values)
 freqs = fft.rfftfreq(len(data), d=1)
+coam = np.argmax(np.absolute(c[1:len(c)//2]))+1
+print('Massimo PS CO: {:f} - Freq {:f} - Periodo: {:d}'.format(np.absolute(c[coam])**2, freqs[coam], int(1/freqs[coam])) )
 '''
 frequenza e potenza
+'''
 plt.plot(freqs[:len(data)//2],np.absolute(c[:len(data)//2]**2))
 plt.xlabel('Freq. [Hz]')
 plt.ylabel('CO')
 plt.xscale('log')
 plt.yscale('log')
-'''
-tempi=1/freqs
-plt.plot(tempi[:len(data)//2],data['mean_co_ug/m3'][:len(data)//2], label='pm2p5')
-plt.xlabel('Freq. [Hz]')
-plt.ylabel('inquinanti')
-plt.xscale('log')
+plt.show()
+ #  le frequenze sono ordinate secondo l'ordine [0-->fmax, -fmax, 0[
+            #  per produrre un grafico corretto si possono riordinare le frequenze con fft.fftshift
+cofrshift = fft.fftshift(freqs)
+copsshift = fft.fftshift(c)
+plt.subplots(figsize=(10,7))
+plt.title('CO')
+plt.plot(cofrshift, copsshift)
+#plt.xscale('log')
 plt.yscale('log')
-plt.legend(loc='lower left')
+plt.xlabel(r'f [$d^{-1}$]')
+plt.ylabel(r'$|c_{FFT}|^2$')
 plt.show()
 
-plt.show()
+
